0933d70226b43fd7375ab779bfb95aff593076fc
AkubaruF <112474451+AkubaruF@users.noreply.github.com>
2025-05-09 10:50:25 +0700

    compiled ewallet

--- modules/TicketingSystem.payment.ewallet/TicketingSystem/payment/ewallet/model/PaymentImpl.java
+++ modules/TicketingSystem.payment.ewallet/TicketingSystem/payment/ewallet/model/PaymentImpl.java
@@ -12,24 +12,56 @@ import TicketingSystem.payment.core.PaymentDecorator;
 import TicketingSystem.payment.core.Payment;
 import TicketingSystem.payment.core.PaymentComponent;
 
-@Entity(name="payment_ewallet")
-@Table(name="payment_ewallet")
+import TicketingSystem.bundling.core.BundlingImpl;
+import TicketingSystem.ticket.core.TicketImpl;
+
+@Entity(name = "payment_ewallet")
+@Table(name = "payment_ewallet")
 public class PaymentImpl extends PaymentDecorator {
 
 	public PaymentImpl() {
-        super();
-        this.objectName = PaymentImpl.class.getName();
-    }
+		super();
+		this.objectName = PaymentImpl.class.getName();
+	}
 
-	
-	public PaymentImpl(PaymentComponent record, ) {
+	public PaymentImpl(PaymentComponent record) {
 		super(record);
 		this.objectName = PaymentImpl.class.getName();
 	}
 
-
 	public void pay() {
 		// TODO: implement this method
+		System.out.println("ewallet pay() called in service; this should ideally be in entity logic.");
+	}
+
+	@Override
+	public int getAmount() {
+		return this.amount;
+	}
+
+	@Override
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public void setTicketimpl(TicketImpl ticketimpl) {
+		this.ticketimpl = ticketimpl;
+	}
+
+	@Override
+	public TicketImpl getTicketimpl() {
+		return this.ticketimpl;
+	}
+
+	@Override
+	public void setBundlingimpl(BundlingImpl bundlingimpl) {
+		this.bundlingimpl = bundlingimpl;
+	}
+
+	@Override
+	public BundlingImpl getBundlingimpl() {
+		return this.bundlingimpl;
 	}
 
 }
--- modules/TicketingSystem.payment.ewallet/TicketingSystem/payment/ewallet/service/PaymentServiceImpl.java
+++ modules/TicketingSystem.payment.ewallet/TicketingSystem/payment/ewallet/service/PaymentServiceImpl.java
@@ -7,14 +7,55 @@ import vmj.routing.route.VMJExchange;
 import TicketingSystem.payment.core.PaymentServiceDecorator;
 import TicketingSystem.payment.core.PaymentImpl;
 import TicketingSystem.payment.core.PaymentServiceComponent;
+import TicketingSystem.payment.core.Payment;
 
 public class PaymentServiceImpl extends PaymentServiceDecorator {
-    public PaymentServiceImpl (PaymentServiceComponent record) {
+    public PaymentServiceImpl(PaymentServiceComponent record) {
         super(record);
     }
 
-    
-	public void pay() {
-		// TODO: implement this method
-	}
+    public void pay() {
+        // TODO: implement this method
+        System.out.println("ewallet pay() called in service; this should ideally be in entity logic.");
+    }
+
+    public List<HashMap<String, Object>> savePayment(VMJExchange vmjExchange) {
+        return record.savePayment(vmjExchange);
+    }
+
+    public Payment createPayment(Map<String, Object> requestBody) {
+        return record.createPayment(requestBody);
+    }
+
+    public Payment createPayment(Map<String, Object> requestBody, Map<String, Object> response) {
+        return record.createPayment(requestBody, response);
+    }
+
+    public HashMap<String, Object> getPayment(Map<String, Object> requestBody) {
+        return record.getPayment(requestBody);
+    }
+
+    public List<HashMap<String, Object>> getAllPayment(Map<String, Object> requestBody) {
+        return record.getAllPayment(requestBody);
+    }
+
+    public HashMap<String, Object> updatePayment(Map<String, Object> requestBody) {
+        return record.updatePayment(requestBody);
+    }
+
+    public List<HashMap<String, Object>> transformListToHashMap(List<Payment> List) {
+        return record.transformListToHashMap(List);
+    }
+
+    public List<HashMap<String, Object>> deletePayment(Map<String, Object> requestBody) {
+        return record.deletePayment(requestBody);
+    }
+
+    public List<HashMap<String, Object>> savePayment(Map<String, Object> requestBody) {
+        return record.savePayment(requestBody);
+    }
+
+    public HashMap<String, Object> getPaymentById(UUID id) {
+        return record.getPaymentById(id);
+    }
 }
