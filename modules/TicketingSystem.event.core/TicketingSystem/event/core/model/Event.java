eb73665ecb77dc6671ac1fef821c3054ae79f76f
AkubaruF <112474451+AkubaruF@users.noreply.github.com>
2025-05-09 08:47:33 +0700

    event.core compiled

--- modules/TicketingSystem.event.core/TicketingSystem/event/core/model/Event.java
+++ modules/TicketingSystem.event.core/TicketingSystem/event/core/model/Event.java
@@ -1,8 +1,29 @@
 package TicketingSystem.event.core;
+
 import vmj.routing.route.Route;
 import vmj.routing.route.VMJExchange;
 import java.util.*;
 
 public interface Event {
 	HashMap<String, Object> toHashMap();
+
+	public UUID getIdEvent();
+
+	public void setIdEvent(UUID idEvent);
+
+	public String getName();
+
+	public void setName(String name);
+
+	public String getDate();
+
+	public void setDate(String date);
+
+	public String getLocation();
+
+	public void setLocation(String location);
+
+	public String getDescription();
+
+	public void setDescription(String description);
 }
